a638e94fd674f931ac1e3c44a244aa15631b08d0
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-08 11:36:49 +0100

    Refactor pawn movement code

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -14,27 +16,26 @@ namespace Chessington.GameEngine.Pieces
             
             var possibleMoves = new List<Square>();
 
+            Square oneSquareInFront;
+            Square twoSquaresInFront;
+
             if (Player == Player.White)
             {
-                var oneSquareInFront = new Square(currentPosition.Row - 1, currentPosition.Col);
-
-                if (board.GetPiece(oneSquareInFront) == null)
-                {
-                    possibleMoves.Add(oneSquareInFront);
-                    if (PreviousPosition == null)
-                        possibleMoves.Add(new Square(currentPosition.Row - 2, currentPosition.Col));
-                }
+                oneSquareInFront = new Square(currentPosition.Row - 1, currentPosition.Col);
+                twoSquaresInFront = new Square(currentPosition.Row - 2, currentPosition.Col);
             }
-            else if (Player == Player.Black)
+            else
             {
-                var oneSquareInFront = new Square(currentPosition.Row + 1, currentPosition.Col);
+                oneSquareInFront = new Square(currentPosition.Row + 1, currentPosition.Col);
+                twoSquaresInFront = new Square(currentPosition.Row + 2, currentPosition.Col);
+            }
 
-                if (board.GetPiece(oneSquareInFront) == null)
-                {
-                    possibleMoves.Add(oneSquareInFront);
-                    if (PreviousPosition == null)
-                        possibleMoves.Add(new Square(currentPosition.Row + 2, currentPosition.Col));
-                }
+            if (board.GetPiece(oneSquareInFront) == null)
+            {
+                possibleMoves.Add(oneSquareInFront);
+                
+                if (PreviousPosition == null)
+                    possibleMoves.Add(twoSquaresInFront);
             }
 
             // Remove positions where a piece already resides
