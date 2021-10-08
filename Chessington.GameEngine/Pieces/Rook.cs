8d4735b576901dc0617e8538f027b64cbb555cf2
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-08 14:01:16 +0100

    Added rook moving vertically

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,28 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentPosition = board.FindPiece(this);
+
+            var possibleMoves = new List<Square>();
+
+            // Add vertical moves
+            for (int pos = currentPosition.Row + 1; board.IsSquareInBoard(Square.At(pos, 0)); pos++)
+            {
+                possibleMoves.Add(Square.At(pos, currentPosition.Col));
+
+                if (board.GetPiece(Square.At(pos, currentPosition.Col)) != null)
+                    break;
+            }
+            
+            for (int pos = currentPosition.Row - 1; board.IsSquareInBoard(Square.At(pos, 0)); pos--)
+            {
+                possibleMoves.Add(Square.At(pos, currentPosition.Col));
+
+                if (board.GetPiece(Square.At(pos, currentPosition.Col)) != null)
+                    break;
+            }
+            
+            return possibleMoves;
         }
     }
 }
