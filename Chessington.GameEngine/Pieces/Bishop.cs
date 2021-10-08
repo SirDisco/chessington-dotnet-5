e4941a002233aee3b58a1ad41ae957e9c3501966
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-08 14:34:58 +0100

    Added bishop movement code

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -6,11 +7,46 @@ namespace Chessington.GameEngine.Pieces
     public class Bishop : Piece
     {
         public Bishop(Player player)
-            : base(player) { }
+            : base(player)
+        {
+            _directions = new Tuple<int, int>[4];
+            _directions[0] = new Tuple<int, int>(1, 1);
+            _directions[1] = new Tuple<int, int>(1, -1);
+            _directions[2] = new Tuple<int, int>(-1, 1);
+            _directions[3] = new Tuple<int, int>(-1, -1);
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentPosition = board.FindPiece(this);
+
+            var possibleMoves = new List<Square>();
+
+            foreach (var direction in _directions)
+            {
+                var row = currentPosition.Row + direction.Item1;
+                var col = currentPosition.Col + direction.Item2;
+
+                // Loop until we find the edge of the board
+                while (board.IsSquareInBoard(Square.At(row, col)))
+                {
+                    var square = Square.At(row, col);
+                    possibleMoves.Add(square);
+
+                    if (board.GetPiece(square) != null)
+                        break;
+
+                    row += direction.Item1;
+                    col += direction.Item2;
+                }
+            }
+
+            // Remove moves that contain a friendly piece
+            possibleMoves.RemoveAll(s => (board.GetPiece(s) != null) && (board.GetPiece(s).Player == Player));
+
+            return possibleMoves;
         }
+        
+        private Tuple<int, int>[] _directions;
     }
 }
